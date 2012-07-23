4b63eabb00042107aee499fd2f39109963562995
Leonardo Suarez <leonardosuarezn@gmail.com>
2012-07-23 16:58:48 -0430

    Hospital version 1.7

---
+++ application/controllers/visitantes.php
@@ -0,0 +1,51 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Visitantes extends CI_Controller {
+
+	function __construct()
+	{
+		parent::__construct();
+		
+		$this->load->database();
+		$this->load->helper('url');
+		
+		$this->load->library('grocery_CRUD');	
+	}
+	
+	function _example_output($output = null)
+	{
+		$this->load->view('visitante.php',$output);	
+	}
+
+	function index()
+	{
+		try{
+			/* This is only for the autocompletion */
+			$crud = new grocery_CRUD();
+
+			$crud->set_table('tbl_adulto');
+			
+			$crud->columns('fechai_adulto', 'id_adulto', 'cedula_adulto', 'nombre_adulto', 'apellido_adulto', 'fechan_adulto', 'telefono_adulto', 'id_usuario', 'fregistro_adulto');
+			
+			$crud->display_as('id_adulto', 'Codigo');
+			$crud->display_as('cedula_adulto', 'Cedula');
+			$crud->display_as('nombre_adulto', 'Nombres');
+			$crud->display_as('apellido_adulto', 'Apellidos');
+			$crud->display_as('fechan_adulto', 'Fecha de Nacimiento');
+			$crud->display_as('fechai_adulto', 'Fecha de Ingreso');
+			$crud->display_as('telefono_adulto', 'Telefono');
+			$crud->display_as('id_usuario', 'Usuario');
+			$crud->display_as('fregistro_adulto', 'Fecha de Registro');
+
+			$crud->set_relation('id_usuario', 'tbl_usuario', 'nombre_usuario');
+
+			$output = $crud->render();
+			
+			$this->_example_output($output);
+			
+		}catch(Exception $e){
+			show_error($e->getMessage().' --- '.$e->getTraceAsString());
+		}
+	}
+	
+}
---
+++ application/views/visitante.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+		<meta charset="utf-8">
+		<title>Hospital Especial Dr. Alejandro Próspero Reverend</title>
+		<?php foreach($css_files as $file): ?>
+			<link type="text/css" rel="stylesheet" href="<?php echo $file; ?>" />
+		<?php endforeach; ?>
+		<?php foreach($js_files as $file): ?>
+			<script src="<?php echo $file; ?>"></script>
+		<?php endforeach; ?>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link href="<?=base_url()?>css/default.css" rel="stylesheet" type="text/css">
+  		<link href="<?=base_url()?>css/admin.css" rel="stylesheet" type="text/css">
+  	<body>
+		<div id="container">
+			
+			<header>
+				<img border="0" src="<?=base_url()?>img/logo-gob.jpg" />
+			</header>
+			
+			<div id="body">
+				<h1 class="centrado">Hospital Especial Dr. Alejandro Próspero Reverend</h1>
+				<p>
+					<h3 class="centrado">Lista de Pacientes</h3>
+					<p>
+					<?php echo $output; ?>
+					</p>
+				</p>
+			</div>
+			<?php include_once("inc/footer.php");?>
+		</div>
+	</body>
+  	<script type="text/javascript" src="<?=base_url()?>js/application.js"></script>
+</html>
