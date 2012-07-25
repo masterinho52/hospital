763ee51b52be7e4f3968960bce4f0873d0a9f48c
Leonardo Suarez <leonardosuarezn@gmail.com>
2012-07-25 17:29:12 -0430

    Hospital version 1.7

--- inc/header_admin.php
+++ inc/header_admin.php
@@ -6,21 +6,19 @@
 		<li> 
 			<a href="#">Archivo</a> 
 		    <ul class="submenu"> 
-			    <li><a href="<?=base_url()?>adultos">Registro del Adulto Mayor</a></li> 
-			    <li><a href="<?=base_url()?>gruposf">Registro del Grupo Familiar</a></li> 
-			    <li><a href="<?=base_url()?>departamentos">Registro del Departamento</a></li> 
-		        <li><a href="<?=base_url()?>cargos">Registro del Cargo</a></li> 
-		        <li><a href="<?=base_url()?>personales">Registro del Personal</a></li> 
-
-		        <?php if ($this->session->userdata("tipo_usuario")==0) { ?>           
+		    	<?php if ($this->session->userdata("tipo_usuario")==0) { ?>           
+		        	<li><a href="<?=base_url()?>usuarios">Registro de Usuarios</a></li>
 		        	<li><hr></li> 
-		        	<li><a href="<?=base_url()?>usuarios">Registro de Usuarios</a></li> 
-		        <?php } ?>
+		        <?php } if (($this->session->userdata("tipo_usuario")==0) or ($this->session->userdata("tipo_usuario")==1) or ($this->session->userdata("tipo_usuario")==2)) { ?>        
+		        <li><a href="<?=base_url()?>departamentos">Registro del Departamento</a></li>
+				<li><a href="<?=base_url()?>cargos">Registro del Cargo</a></li>
+		        <li><a href="<?=base_url()?>personales">Registro del Personal</a></li> 
+		        <li><a href="<?=base_url()?>adultos">Registro del Adulto Mayor</a></li> 
+			    <li><a href="<?=base_url()?>gruposf">Registro del Grupo Familiar</a></li> 
+		   		<?php } ?>
 		   	</ul> 
 		</li> 
-		<li><a href="<?=base_url()?>login/logout">Salir</a></li>
-		<li style="with:200px;"></li>
-		<li><a href="#"><?php if ($this->session->userdata("tipo_usuario")==0) { echo "Bienvenido, ".$this->session->userdata("tipo_usuario")." del Sistema" ; } if ($this->session->userdata("tipo_usuario")==1) { echo "Bienvenido, ".$this->session->userdata("tipo_usuario")." del Sistema"; } if ($this->session->userdata("tipo_usuario")==2) { echo "Bienvenido, ".$this->session->userdata("tipo_usuario")." Sistema"; } ?></a></li> 
+		<li><a href="<?=base_url()?>login/logout">Salir</a></li> 
 		<!-- <li> 
 		    <a href="#">Códigos</a> 
 			<ul class="submenu"> 
@@ -33,4 +31,5 @@
 		  	</ul> 
 		</li> -->
 	</ul> 
+	<div align="right" style="margin-right:10px;" class="usuario"><?php if ($this->session->userdata("tipo_usuario")==0) { echo "Bienvenido, ".$this->session->userdata("tipo_usuario")." del Sistema" ; } if ($this->session->userdata("tipo_usuario")==1) { echo "Bienvenido, ".$this->session->userdata("tipo_usuario")." del Sistema"; } if ($this->session->userdata("tipo_usuario")==2) { echo "Bienvenido, ".$this->session->userdata("tipo_usuario")." Sistema"; } ?></div>
 </header>
