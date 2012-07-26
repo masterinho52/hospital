b080d3d83b87d39b73df353a2ffb60a13e7524fb
Johnmer Bencomo <bjohnmer@gmail.com>
2012-07-26 10:40:15 -0430

    Cambios Locales

--- inc/header_admin.php
+++ inc/header_admin.php
@@ -6,16 +6,17 @@
 		<li> 
 			<a href="#">Archivo</a> 
 		    <ul class="submenu"> 
-		    	<?php if ($this->session->userdata("tipo_usuario")==0) { ?>           
+		    	<?php 
+		    	if ($this->session->userdata("tipo_usuario")=="Administrador") { ?>           
 		        	<li><a href="<?=base_url()?>usuarios">Registro de Usuarios</a></li>
 		        	<li><hr></li> 
-		        <?php } if (($this->session->userdata("tipo_usuario")==0) or ($this->session->userdata("tipo_usuario")==1) or ($this->session->userdata("tipo_usuario")==2)) { ?>        
+		        <?php 
+		    	} ?>        
 		        <li><a href="<?=base_url()?>departamentos">Registro del Departamento</a></li>
 				<li><a href="<?=base_url()?>cargos">Registro del Cargo</a></li>
 		        <li><a href="<?=base_url()?>personales">Registro del Personal</a></li> 
 		        <li><a href="<?=base_url()?>adultos">Registro del Adulto Mayor</a></li> 
-			    <li><a href="<?=base_url()?>gruposf">Registro del Grupo Familiar</a></li> 
-		   		<?php } ?>
+			    <li><a href="<?=base_url()?>gruposf">Registro del Grupo Familiar</a></li>
 		   	</ul> 
 		</li> 
 		<li><a href="<?=base_url()?>login/logout">Salir</a></li> 
